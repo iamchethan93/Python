010274e785d9fa4b16aba11995f441f702676d1f
Chethan <chethangowda59@gmail.com>
2026-01-11 16:34:17 +0530

    Learnings of 11/01/2026

---
+++ typecasting.py
@@ -0,0 +1,7 @@
+a = "1"
+b = "2"
+print(a+b)
+
+a = int(a)
+b = int(b)
+print(a+b)
---
+++ userInput.py
@@ -0,0 +1,9 @@
+# Input function takes input from user and returns it as a string.
+x = input("Enter first number :")
+y = input("Enter second number :")
+print( x + y)
+
+# While using numbers. its important to typecast to correct data type.
+x = input("Enter first number :")
+y = input("Enter second number :")
+print( int(x) + int(y))
